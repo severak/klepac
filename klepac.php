6fb1dd0ab7f2eb4be12eebe21473047c339333fd
Mikolas Strajt <strajt@dalten.cz>
2026-04-09 01:17:18 +0200

    WIP: command line API

---
+++ klepac.php
@@ -0,0 +1,29 @@
+<?php
+// TODO - zprovoznit - budeme ovládat přes command line
+
+/**
+ * Klepac - minimalist RSS reader.
+ */
+class klepac
+{
+    /**
+     * Adds new url of feed.
+     *
+     * @param string $url URL of RSS feed to add.
+     * @return void
+     */
+    function add($url)
+    {
+
+    }
+
+    /**
+     * Updates all the RSS feeds.
+     *
+     * @return void
+     */
+    function update()
+    {
+
+    }
+}
